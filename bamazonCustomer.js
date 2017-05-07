95fe4b53c1155e88738837f833c9d8ec1e16f7b0
ppreziotti <patpreziotti@gmail.com>
2017-05-07 15:25:05 -0400

    completed bamazon functionality, created bamazonManager.js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,34 +22,45 @@ function displayItems() {
 				"\nItem Id: " + res[i].item_id + "\nProduct: " + res[i].product_name +
 				"\nDepartment: " + res[i].department_name + "\nPrice: " + res[i].price);
 		}
-		askUser();
-	});
-}
-
-function askUser() {
-	inquirer.prompt([
-		{
-			name: "product",
-			message: "Please enter the 'Item Id' of the product you would like to purchase:",
-			validate: function(value) {
-				if (isNaN(value) === false) {
-					return true;
+		inquirer.prompt([
+			{
+				name: "id",
+				message: "Please enter the 'Item Id' of the product you would like to purchase:",
+				validate: function(value) {
+					if (isNaN(value) === false) {
+						return true;
+					}
+					return false;
 				}
-				return false;
-			}
-		},
-		{
-			name: "quantity",
-			message: "Please enter the quantity of the product you would like to purchase:",
-			validate: function(value) {
-				if (isNaN(value) === false) {
-					return true;
+			},
+			{
+				name: "quantity",
+				message: "Please enter the quantity of the product you would like to purchase:",
+				validate: function(value) {
+					if (isNaN(value) === false) {
+						return true;
+					}
+					return false;
 				}
-				return false;
 			}
-		}
-	]).then(function(answers) {
-		console.log("Thank you for your order!");
+		]).then(function(answers) {
+			var chosenItem;
+			for (var i = 0; i < res.length; i++) {
+				if (res[i].item_id === parseInt(answers.id)) {
+					chosenItem = res[i];
+				}
+			}
+			if (chosenItem.stock_quantity >= answers.quantity) {
+				connection.query("UPDATE products SET ? WHERE ?", [{stock_quantity: parseInt(chosenItem.stock_quantity) - parseInt(answers.quantity)}, {item_id: chosenItem.item_id}], function(err, res) {
+					if (err) throw err;
+					var totalCost = chosenItem.price * answers.quantity;
+					console.log("Thank you for your purchase! Your total cost is $" + totalCost + ".");
+				});
+			}
+			else {
+				console.log("We are very sorry. There is insufficient stock for your order.");
+			}
+		});
 	});
 }
 
