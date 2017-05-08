1633e9540a968e485f11e89af4ee74798f5a3ece
ppreziotti <patpreziotti@gmail.com>
2017-05-08 06:40:43 -0400

    completed bamazonManager functions, still need to polish up code

--- bamazonManager.js
+++ bamazonManager.js
@@ -24,6 +24,8 @@ function displayOptions() {
 			"Add New Product"]
 		}
 	]).then(function(answers) {
+		// Selects all products for sale from the products table and then displays them
+		// with item id, product name, price, and stock quantity
 		if (answers.options === "View Products for Sale") {
 			connection.query("SELECT * FROM products", function(err, res) {
 				if (err) throw err;
@@ -49,13 +51,81 @@ function displayOptions() {
 			});
 		}
 		else if (answers.options === "Add to Inventory") {
-			// Display all items and allow manager to pick which item to add inventory to
+			// Selects all products for sale from the products table and then displays
+			// them and allows the manager to pick which item to add inventory to
+			connection.query("SELECT * FROM products", function(err, res) {
+				if (err) throw err;
+				for (var i = 0; i < res.length; i++) {
+					console.log("---------------------------------------" + 
+						"\nItem Id: " + res[i].item_id + "\nProduct: " + 
+						res[i].product_name + "\nPrice: " + res[i].price + 
+						"\nQuantity in Stock: " + res[i].stock_quantity);
+				}
+				inquirer.prompt([
+					{
+						name: "id",
+						message: "Please enter the 'Item Id' of the product you would like to increase inventory for:",
+						validate: function(value) {
+							if (isNaN(value) === false) {
+								return true;
+							}
+							return false;
+						}
+					},
+					{
+						name: "quantity",
+						message: "Please enter the quantity of the product you would like to add to inventory:",
+						validate: function(value) {
+							if (isNaN(value) === false) {
+								return true;
+							}
+							return false;
+						}
+					}
+				]).then(function(answers) {
+					var chosenItem;
+					for (var i = 0; i < res.length; i++) {
+						if (res[i].item_id === parseInt(answers.id)) {
+							chosenItem = res[i];
+						}
+					}
+					connection.query("UPDATE products SET ? WHERE ?", [{stock_quantity: parseInt(chosenItem.stock_quantity) + parseInt(answers.quantity)}, {item_id: chosenItem.item_id}], function(err, res) {
+						if (err) throw err;
+						console.log("The product quantity has been succesfully updated.");
+					});
+				});
+			});
 		}
 		else if (answers.options === "Add New Product") {
-			// Allow the manager to add a new product to the products table in the bamazon db
+			// Allows the manager to add a new product to the products table in the bamazon db
+			inquirer.prompt([
+				{
+					name: "name",
+					message: "Please enter the product name:"
+				},
+				{
+					name: "department",
+					message: "Please enter the product department: "
+				},
+				{
+					name: "price",
+					message: "Please enter the product price:"
+				},
+				{
+					name: "quantity",
+					message: "Please enter the quantity in stock:"
+				}
+			]).then(function(answers) {
+				connection.query("INSERT INTO products SET ?", {product_name: answers.name,
+					department_name: answers.department, price: answers.price, 
+					stock_quantity: answers.quantity}, function(err, res) {
+						if (err) throw (err);
+						console.log("Product added succesfully!");
+					});	
+			});
 		}
 	});
 }
 
-// Execute displayOptions() to start program
+// Executes displayOptions() to start program
 displayOptions();
