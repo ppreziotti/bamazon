5ad68913e716de3674768e0bb902f65e81d976df
ppreziotti <patpreziotti@gmail.com>
2017-05-08 21:30:28 -0400

    code cleanup

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+// Creates connection to the bamazon database using mySQL
 var connection = mysql.createConnection({
 	host: 'localhost',
 	post: 3306,
@@ -13,15 +14,18 @@ connection.connect(function(err) {
 	if (err) throw err;
 });
 
-// Displays all items available with item id, product name, department name, and price
+// Selects and displays all items from the products table
+// Item id, product name, department name, and price are all shown
 function displayItems() {
 	connection.query("SELECT * FROM products", function(err, res) {
 		if (err) throw err;
 		for (var i = 0; i < res.length; i++) {
 			console.log("--------------------------------------------------" + 
-				"\nItem Id: " + res[i].item_id + "\nProduct: " + res[i].product_name +
-				"\nDepartment: " + res[i].department_name + "\nPrice: " + res[i].price);
+			"\nItem Id: " + res[i].item_id + "\nProduct: " + res[i].product_name +
+			"\nDepartment: " + res[i].department_name + "\nPrice: " + res[i].price);
 		}
+		// User inputs the item id of the product they would like to purchase along with
+		// the quantity of the product
 		inquirer.prompt([
 			{
 				name: "id",
@@ -50,15 +54,46 @@ function displayItems() {
 					chosenItem = res[i];
 				}
 			}
+			// If there is enough of the product available to fulfill the user's purchase
+			// then the use is given the total cost of their purchase in the stock quantity
+			// in the product table is updated
 			if (chosenItem.stock_quantity >= answers.quantity) {
 				connection.query("UPDATE products SET ? WHERE ?", [{stock_quantity: parseInt(chosenItem.stock_quantity) - parseInt(answers.quantity)}, {item_id: chosenItem.item_id}], function(err, res) {
 					if (err) throw err;
 					var totalCost = chosenItem.price * answers.quantity;
 					console.log("Thank you for your purchase! Your total cost is $" + totalCost + ".");
+					inquirer.prompt([
+						{
+							name: "confirm",
+							type: "confirm",
+							message: "Would you like to order something else?"
+						}
+					]).then(function(answer) {
+						if (answer.confirm === true) {
+							displayItems();
+						}
+						else {
+							console.log("Thank you for shopping with us. Come back again soon!");
+						}
+					});
 				});
 			}
 			else {
 				console.log("We are very sorry. There is insufficient stock for your order.");
+				inquirer.prompt([
+					{
+						name: "confirm",
+						type: "confirm",
+						message: "Would you like to order something else?"
+					}
+				]).then(function(answer) {
+					if (answer.confirm === true) {
+						displayItems();
+					}
+					else {
+						console.log("Thank you for shopping with us. Come back again soon!");
+					}
+				});
 			}
 		});
 	});
--- bamazonManager.js
+++ bamazonManager.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+// Creates connection to the bamazon database using mySQL
 var connection = mysql.createConnection({
 	host: 'localhost',
 	post: 3306,
@@ -13,7 +14,7 @@ connection.connect(function(err) {
 	if (err) throw err;
 });
 
-// Displays options for the manager
+// Displays a list of options for the manager to chose from
 function displayOptions() {
 	inquirer.prompt([
 		{
@@ -24,106 +25,180 @@ function displayOptions() {
 			"Add New Product"]
 		}
 	]).then(function(answers) {
-		// Selects all products for sale from the products table and then displays them
-		// with item id, product name, price, and stock quantity
 		if (answers.options === "View Products for Sale") {
-			connection.query("SELECT * FROM products", function(err, res) {
-				if (err) throw err;
-				for (var i = 0; i < res.length; i++) {
-					console.log("---------------------------------------" + 
-						"\nItem Id: " + res[i].item_id + "\nProduct: " + 
-						res[i].product_name + "\nPrice: " + res[i].price + 
-						"\nQuantity in Stock: " + res[i].stock_quantity);
-				}
-			});
+			viewProducts();
 		}
 		else if (answers.options === "View Low Inventory") {
-			connection.query("SELECT * FROM products", function(err, res) {
-				if (err) throw err;
-				for (var i = 0; i < res.length; i++) {
-					if (res[i].stock_quantity < 5) {
-						console.log("---------------------------------------" + 
-							"\nItem Id: " + res[i].item_id + "\nProduct: " + 
-							res[i].product_name + "\nPrice: " + res[i].price + 
-							"\nQuantity in Stock: " + res[i].stock_quantity);
-					}
-				}				
-			});
+			lowInventory();
 		}
 		else if (answers.options === "Add to Inventory") {
-			// Selects all products for sale from the products table and then displays
-			// them and allows the manager to pick which item to add inventory to
-			connection.query("SELECT * FROM products", function(err, res) {
-				if (err) throw err;
-				for (var i = 0; i < res.length; i++) {
-					console.log("---------------------------------------" + 
-						"\nItem Id: " + res[i].item_id + "\nProduct: " + 
-						res[i].product_name + "\nPrice: " + res[i].price + 
-						"\nQuantity in Stock: " + res[i].stock_quantity);
-				}
-				inquirer.prompt([
-					{
-						name: "id",
-						message: "Please enter the 'Item Id' of the product you would like to increase inventory for:",
-						validate: function(value) {
-							if (isNaN(value) === false) {
-								return true;
-							}
-							return false;
-						}
-					},
-					{
-						name: "quantity",
-						message: "Please enter the quantity of the product you would like to add to inventory:",
-						validate: function(value) {
-							if (isNaN(value) === false) {
-								return true;
-							}
-							return false;
-						}
-					}
-				]).then(function(answers) {
-					var chosenItem;
-					for (var i = 0; i < res.length; i++) {
-						if (res[i].item_id === parseInt(answers.id)) {
-							chosenItem = res[i];
-						}
-					}
-					connection.query("UPDATE products SET ? WHERE ?", [{stock_quantity: parseInt(chosenItem.stock_quantity) + parseInt(answers.quantity)}, {item_id: chosenItem.item_id}], function(err, res) {
-						if (err) throw err;
-						console.log("The product quantity has been succesfully updated.");
-					});
-				});
-			});
+			addInventory();
 		}
 		else if (answers.options === "Add New Product") {
-			// Allows the manager to add a new product to the products table in the bamazon db
+			addProduct();
+		}
+	});
+}
+
+// Selects all products for sale from the products table and then displays them
+// with item id, product name, price, and stock quantity
+function viewProducts() {
+	connection.query("SELECT * FROM products", function(err, res) {
+		if (err) throw err;
+		for (var i = 0; i < res.length; i++) {
+			console.log("---------------------------------------" + 
+			"\nItem Id: " + res[i].item_id + "\nProduct: " + 
+			res[i].product_name + "\nPrice: " + res[i].price + 
+			"\nQuantity in Stock: " + res[i].stock_quantity);
+		}
+		inquirer.prompt([
+			{
+				name: "confirm",
+				type: "confirm",
+				message: "Would like to make another selection?"
+			}
+		]).then(function(answer) {
+			if (answer.confirm === true) {
+				displayOptions();
+			}
+			else {
+				console.log("Thank you. Come back again soon.");
+			}
+		});
+	});
+}
+
+// Displays the item id, product name, price and stock quantity for any product
+// with less than 5 items left in inventory
+function lowInventory() {
+	connection.query("SELECT * FROM products", function(err, res) {
+		if (err) throw err;
+		for (var i = 0; i < res.length; i++) {
+			if (res[i].stock_quantity < 5) {
+				console.log("---------------------------------------" + 
+				"\nItem Id: " + res[i].item_id + "\nProduct: " + 
+				res[i].product_name + "\nPrice: " + res[i].price + 
+				"\nQuantity in Stock: " + res[i].stock_quantity);
+			}
+		}
+		inquirer.prompt([
+			{
+				name: "confirm",
+				type: "confirm",
+				message: "Would like to make another selection?"
+			}
+		]).then(function(answer) {
+			if (answer.confirm === true) {
+				displayOptions();
+			}
+			else {
+				console.log("Thank you. Come back again soon.");
+			}
+		});				
+	});
+}
+
+// Selects all products for sale from the products table and then displays
+// them and allows the manager to pick which item to add inventory to
+function addInventory() {
+	connection.query("SELECT * FROM products", function(err, res) {
+		if (err) throw err;
+		for (var i = 0; i < res.length; i++) {
+			console.log("---------------------------------------" + 
+			"\nItem Id: " + res[i].item_id + "\nProduct: " + 
+			res[i].product_name + "\nPrice: " + res[i].price + 
+			"\nQuantity in Stock: " + res[i].stock_quantity);
+		}
+		inquirer.prompt([
+			{
+				name: "id",
+				message: "Please enter the 'Item Id' of the product you would like to increase inventory for:",
+				validate: function(value) {
+					if (isNaN(value) === false) {
+						return true;
+					}
+					return false;
+				}
+			},
+			{
+				name: "quantity",
+				message: "Please enter the quantity of the product you would like to add to inventory:",
+				validate: function(value) {
+					if (isNaN(value) === false) {
+						return true;
+					}
+					return false;
+				}
+			}
+		]).then(function(answers) {
+			var chosenItem;
+			for (var i = 0; i < res.length; i++) {
+				if (res[i].item_id === parseInt(answers.id)) {
+					chosenItem = res[i];
+				}
+			}
+			connection.query("UPDATE products SET ? WHERE ?", [{stock_quantity: parseInt(chosenItem.stock_quantity) + parseInt(answers.quantity)}, {item_id: chosenItem.item_id}], function(err, res) {
+				if (err) throw err;
+				console.log("The product quantity has been succesfully updated.");
+				inquirer.prompt([
+					{
+						name: "confirm",
+						type: "confirm",
+						message: "Would like to make another selection?"
+					}
+				]).then(function(answer) {
+					if (answer.confirm === true) {
+						displayOptions();
+					}
+					else {
+						console.log("Thank you. Come back again soon.");
+					}
+				});
+			});
+		});
+	});
+}
+
+// Allows the manager to add a new product to the products table in the bamazon db
+function addProduct() {
+	inquirer.prompt([
+		{
+			name: "name",
+			message: "Please enter the product name:"
+		},
+		{
+			name: "department",
+			message: "Please enter the product department:"
+		},
+		{
+			name: "price",
+			message: "Please enter the product price:"
+		},
+		{
+			name: "quantity",
+			message: "Please enter the quantity in stock:"
+		}
+	]).then(function(answers) {
+		connection.query("INSERT INTO products SET ?", {product_name: answers.name,
+		department_name: answers.department, price: answers.price, 
+		stock_quantity: answers.quantity}, function(err, res) {
+			if (err) throw (err);
+			console.log("Product added succesfully!");
 			inquirer.prompt([
 				{
-					name: "name",
-					message: "Please enter the product name:"
-				},
-				{
-					name: "department",
-					message: "Please enter the product department: "
-				},
-				{
-					name: "price",
-					message: "Please enter the product price:"
-				},
-				{
-					name: "quantity",
-					message: "Please enter the quantity in stock:"
+					name: "confirm",
+					type: "confirm",
+					message: "Would like to make another selection?"
 				}
-			]).then(function(answers) {
-				connection.query("INSERT INTO products SET ?", {product_name: answers.name,
-					department_name: answers.department, price: answers.price, 
-					stock_quantity: answers.quantity}, function(err, res) {
-						if (err) throw (err);
-						console.log("Product added succesfully!");
-					});	
+			]).then(function(answer) {
+				if (answer.confirm === true) {
+					displayOptions();
+				}
+				else {
+					console.log("Thank you. Come back again soon.");
+				}
 			});
-		}
+		});	
 	});
 }
 
